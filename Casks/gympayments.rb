e2028286a3f1f83a6b2c958a8d4169012f48fe85
Selvasuriya <selva.patchamal@zucisystem.com>
2026-02-19 21:06:58 +0530

    made release public

--- Casks/gympayments.rb
+++ Casks/gympayments.rb
@@ -2,10 +2,10 @@ cask "gympayments" do
   version "1.0"
   sha256 "sha256:65c17902ce97d7a0558ecc9a1c1251063bd152e09f691061c67fab48b314593e"
 
-  url "https://github.com/selva-bizzgit/gym-payments/releases/download/first-release/GymPayments-1.0.dmg"
+  url "https://github.com/selva-bizzgit/homebrew-gympayments/releases/download/first-release/GymPayments-1.0.dmg"
   name "GymPayments"
   desc "Offline Gym Billing System"
-  homepage "https://github.com/selva-bizzgit/gym-payments"
+  homepage "https://github.com/selva-bizzgit/homebrew-gympayments"
 
   app "GymPayments.app"
 end
